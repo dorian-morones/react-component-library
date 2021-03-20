e44e2f1f4415da002abea4b3c313deaee525e72d
Dorian Morones <dorianm393@gmail.com>
2021-03-20 11:30:56 -0700

    Color Palette

---
+++ src/ColorPalette/Colors.tsx
@@ -0,0 +1,25 @@
+
+const MainColors = {
+  red: '#F5222D',
+  volcano: '#FA541C',
+  orange: '#FA8C16',
+  gold: '#FAAD14',
+  yellow: '#FADB14',
+  lime: '#A0D911',
+  green: '#52C41A',
+  cyan: '#13C2C2',
+  blue: '#1890FF',
+  geekblue: '#2F54EB',
+  purple: '#722ED1',
+  magenta: '#EB2F96',
+  grey: '#666666',
+  black: '#000000',
+  white: '#ffffff'
+}
+
+const Colors = {
+  MainColors: MainColors
+}
+
+
+export default Colors;
